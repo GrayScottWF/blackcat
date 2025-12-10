ff8eda678c7d36e7ee001f514d08be78dce5de9d
Scott <s921208s@gmail.com>
2025-12-10 17:54:29 +0800

    navbar drop down done

--- src/assets/components/navbar/StyledNavbar.ts
+++ src/assets/components/navbar/StyledNavbar.ts
@@ -13,26 +13,39 @@ export const StyledLogo = styled.img`
     cursor: pointer;
 `
 
-export const StyledItems = styled.ul`
+export const StyledNavItems = styled.ul`
     display: flex;
     justify-content: space-around;
     text-align: center;
+    align-items: center;
     width: 50%;
+    height: 90px;
 `
 
-export const StyledItemGap = styled.div`
+export const StyledNavItemsGap = styled.div`
     width: 2px;
     height: 22px;
     background-color: #ccc;
     
 `
+export const StyledNavItem = styled.li`
+    &:hover .DropDown{
+        display: flex;
+    }
+`
 
-export const StyledItem = styled.li`
+export const StyledNavItemTitle = styled.span`
+    position: relative;
     font-size: 20px;
     margin: 0 2.5vh;
-
+    transition: all  0.2s;
 
     cursor: pointer;
+
+    &:hover .DropDown{
+        display: flex;
+    }
+
 
     &:after{
         content: '';
@@ -78,6 +91,8 @@ export const StyledSearchBtn = styled.button`
     width: 45px;
     border: 1px solid #ccc;
     border-radius: 50%;
+    background-color: #fff;
+
     cursor: pointer;
 
     &:after{
@@ -86,16 +101,75 @@ export const StyledSearchBtn = styled.button`
 `
 
 export const StyledDropDown = styled.div`
+    display: none;
+    /* display: flex; */
+    max-height: 352px;
+    /* height: 352px; */
+    width: 100%;
+    background-color: rgba(240, 240, 240, 0.9);
+
+
+    position: absolute;
+    top: 90px;
+    left: 0;
+    justify-content: center;
 
 `
 
-export const StyledLink = styled.div`
+export const StyledDropDownMenu = styled.ul`
+    display: flex;
+    flex-direction: row;
+    padding: 24px 0;
+    /* padding-top: 24px; */
+    /* justify-content: left; */
 
 `
 
-export const Styleds = styled.div`
+export const StyledDropDownMenuItem = styled.li`
+    /* display: flex; */
+    /* justify-content: left; */
+    margin: 0 24px;
 
 `
+
+export const StyledMenuTitle = styled.div`
+
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 28px;
+    text-align:left;
+    white-space: nowrap;
+`
+
+export const StyledMenuItem1 = styled.ul`
+    font-size: 16px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    max-height: 352px;
+    justify-content: left;
+    height: 250px;
+    text-align: left;
+`
+export const StyledMenuItem2 = styled.ul`
+    font-size: 16px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    max-height: 352px;
+    justify-content: left;
+    height: 130px;
+    text-align: left;
+`
+
+
+export const StyledMenuLink = styled.li`
+    font-weight: normal;
+    font-size: 16px;
+    margin-top: 16px;
+    padding-right: 64px;
+`
+
 
 export const Styled = styled.div`
 
--- src/assets/components/navbar/navbar.tsx
+++ src/assets/components/navbar/navbar.tsx
@@ -1,40 +1,224 @@
 
+import { Container } from "react-bootstrap";
 import { 
     StyledNavbar,
     StyledLogo,
-    StyledItems,
-    StyledItem,
-    StyledItemGap,
+    StyledNavItems,
+    StyledNavItem,
+    StyledNavItemsGap,
     StyledSearch,
     StyledText,
     StyledSearchBtn,
+    StyledDropDown,
+    StyledDropDownMenu,
+    StyledDropDownMenuItem,
 
+    StyledMenuTitle,
+    StyledMenuLink,
+    StyledNavItemTitle,
+    StyledMenuItem1,
+    StyledMenuItem2
 
 } from "./StyledNavbar";
 
 import { Link } from "react-router-dom";
 function Navbar() {
     return(
+        <>
         <StyledNavbar>
             <Link to={'/'}>
                 <StyledLogo src="/src/assets/images/Logo.jpg" alt="404" />
             </Link>
-            <StyledItems>
-                <StyledItem>寄件</StyledItem>
-                <StyledItemGap/>
-                <StyledItem>查詢</StyledItem>
-                <StyledItemGap/>
-                <StyledItem>客戶服務</StyledItem>
-                <StyledItemGap/>
-                <StyledItem>個人會員</StyledItem>
-                <StyledItemGap/>
-                <StyledItem>契約客戶專區</StyledItem>
-            </StyledItems>
+            <StyledNavItems>
+                <StyledNavItem>
+                    <StyledNavItemTitle>寄件</StyledNavItemTitle>
+                    <StyledDropDown className="DropDown">
+                        <Container>
+                            <StyledDropDownMenu>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>服務介紹</StyledMenuTitle>
+                                    <StyledMenuItem1>
+                                        <StyledMenuLink>常溫宅急便</StyledMenuLink>
+                                        <StyledMenuLink>低溫宅急便</StyledMenuLink>
+                                        <StyledMenuLink>經濟宅急便</StyledMenuLink>
+                                        <StyledMenuLink>到付宅急便</StyledMenuLink>
+                                        <StyledMenuLink>高爾夫宅急便</StyledMenuLink>
+                                        <StyledMenuLink>醫藥物流服務</StyledMenuLink>
+                                        <StyledMenuLink>宅轉店</StyledMenuLink>
+                                        <StyledMenuLink>快速到店</StyledMenuLink>
+                                        <StyledMenuLink>倉儲服務</StyledMenuLink>
+                                        <StyledMenuLink>黑貓探險隊</StyledMenuLink>
+                                        <StyledMenuLink>金流代收服務</StyledMenuLink>
+                                    </StyledMenuItem1>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>icat網路宅急便</StyledMenuTitle>
+                                    <StyledMenuItem1>
+                                        <StyledMenuLink>服務說明</StyledMenuLink>
+                                        <StyledMenuLink>單筆預約寄件</StyledMenuLink>
+                                        <StyledMenuLink>多筆預約寄件</StyledMenuLink>
+                                        <StyledMenuLink>icat app</StyledMenuLink>
+                                        <StyledMenuLink>訂單管理</StyledMenuLink>
+                                        <StyledMenuLink>配完通知&查詢</StyledMenuLink>
+                                    </StyledMenuItem1>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>寄件指南</StyledMenuTitle>
+                                    <StyledMenuItem1>
+                                        <StyledMenuLink>包裝建議</StyledMenuLink>
+                                        <StyledMenuLink>託運條款</StyledMenuLink>
+                                        <StyledMenuLink>不受理項目</StyledMenuLink>
+                                    </StyledMenuItem1>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>包裝資材</StyledMenuTitle>
+                                    <StyledMenuItem1>
+                                        <StyledMenuLink>包裝資材訂購</StyledMenuLink>
+                                        <StyledMenuLink>訂單管理</StyledMenuLink>
+                                    </StyledMenuItem1>
+                                </StyledDropDownMenuItem>
+                            </StyledDropDownMenu>
+                        </Container>
+                    </StyledDropDown>
+                </StyledNavItem>
+                <StyledNavItemsGap/>
+                <StyledNavItem>
+                    <StyledNavItemTitle>查詢</StyledNavItemTitle>
+                    <StyledDropDown className="DropDown">
+                        <Container>
+                            <StyledDropDownMenu>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>包裹查詢</StyledMenuTitle>
+                                    <StyledMenuItem2>
+                                        <StyledMenuLink>包裹查詢說明</StyledMenuLink>
+                                        <StyledMenuLink>一般包裹查詢</StyledMenuLink>
+                                        <StyledMenuLink>連號包裹查詢</StyledMenuLink>
+                                    </StyledMenuItem2>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>運送及送達時間查詢</StyledMenuTitle>
+                                    <StyledMenuItem2>
+                                        <StyledMenuLink>送達時間說明</StyledMenuLink>
+                                        <StyledMenuLink>運費說明</StyledMenuLink>
+                                    </StyledMenuItem2>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>服務據點查詢</StyledMenuTitle>
+                                    <StyledMenuItem2>
+                                        <StyledMenuLink>黑貓寄取站查詢</StyledMenuLink>
+                                        <StyledMenuLink>合作代收查詢</StyledMenuLink>
+                                    </StyledMenuItem2>
+                                </StyledDropDownMenuItem>
+                                                            </StyledDropDownMenu>
+                        </Container>
+                    </StyledDropDown>
+                </StyledNavItem>
+                <StyledNavItemsGap/>
+                <StyledNavItem>
+                    <StyledNavItemTitle>客戶服務</StyledNavItemTitle>
+                    <StyledDropDown className="DropDown">
+                        <Container>
+                            <StyledDropDownMenu>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>常見問題</StyledMenuTitle>
+                                    <StyledMenuItem2>
+                                        <StyledMenuLink>常見問題</StyledMenuLink>
+                                        <StyledMenuLink>跨境包過消費爭議專區</StyledMenuLink>
+                                        <StyledMenuLink>ezcat下載</StyledMenuLink>
+                                    </StyledMenuItem2>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>聯絡黑貓</StyledMenuTitle>
+                                    <StyledMenuItem2>
+                                        <StyledMenuLink>網路客服</StyledMenuLink>
+                                        <StyledMenuLink>智能客服</StyledMenuLink>
+                                        <StyledMenuLink>自助寄件專線</StyledMenuLink>
+                                    </StyledMenuItem2>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>電子發票</StyledMenuTitle>
+                                    <StyledMenuItem2>
+                                    </StyledMenuItem2>
+                                </StyledDropDownMenuItem>
+                            </StyledDropDownMenu>
+                        </Container>
+                    </StyledDropDown>
+                </StyledNavItem>
+                <StyledNavItemsGap/>
+                <StyledNavItem>
+                    <StyledNavItemTitle>個人會員</StyledNavItemTitle>
+                    <StyledDropDown className="DropDown">
+                        <Container>
+                            <StyledDropDownMenu>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>icat網路宅急便</StyledMenuTitle>
+                                    <StyledMenuItem2>
+                                        <StyledMenuLink>服務說明</StyledMenuLink>
+                                        <StyledMenuLink>單筆預約寄件</StyledMenuLink>
+                                        <StyledMenuLink>多筆預約寄件</StyledMenuLink>
+                                        <StyledMenuLink>會員登入</StyledMenuLink>
+                                        <StyledMenuLink>會員資料維護</StyledMenuLink>
+                                        <StyledMenuLink>訂單管理</StyledMenuLink>
+                                    </StyledMenuItem2>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>LINE</StyledMenuTitle>
+                                    <StyledMenuItem2>
+                                        <StyledMenuLink>服務說明</StyledMenuLink>
+                                        <StyledMenuLink>加入好友</StyledMenuLink>
+                                        <StyledMenuLink>加入會員</StyledMenuLink>
+                                    </StyledMenuItem2>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>綁定uniopen</StyledMenuTitle>
+                                    <StyledMenuItem2>
+                                        <StyledMenuLink>累積/折抵使用辦法</StyledMenuLink>
+                                    </StyledMenuItem2>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>隱私權聲明</StyledMenuTitle>
+                                    <StyledMenuItem2>
+                                    </StyledMenuItem2>
+                                </StyledDropDownMenuItem>
+                            </StyledDropDownMenu>
+                        </Container>
+                    </StyledDropDown>
+                </StyledNavItem>
+                <StyledNavItemsGap/>
+                <StyledNavItem>
+                    <StyledNavItemTitle>契約客戶專區</StyledNavItemTitle>
+                    <StyledDropDown className="DropDown">
+                        <Container>
+                            <StyledDropDownMenu>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>契約客戶專區</StyledMenuTitle>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>契約客戶洽談</StyledMenuTitle>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>包裹狀態查詢介紹</StyledMenuTitle>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>ezcat印單軟體下載</StyledMenuTitle>
+                                </StyledDropDownMenuItem>
+                                <StyledDropDownMenuItem>
+                                    <StyledMenuTitle>SmartCat app 智能宅急便</StyledMenuTitle>
+                                </StyledDropDownMenuItem>
+                            </StyledDropDownMenu>
+                        </Container>
+                    </StyledDropDown>
+                </StyledNavItem>
+            </StyledNavItems>
             <StyledSearch>
                 <StyledText type="text" placeholder="站內搜尋" />
                 <StyledSearchBtn />
             </StyledSearch>
         </StyledNavbar>
+        <StyledDropDown>
+
+        </StyledDropDown>
+        </>
     )
 
 }
